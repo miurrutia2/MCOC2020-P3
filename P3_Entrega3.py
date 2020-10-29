10ac21c3db891f9ee7bf702afe09981cd43b70bc
miurrutia2 <37964599+miurrutia2@users.noreply.github.com>
2020-10-29 19:56:06 -0300

    Update P3_Entrega3.py

--- P3_Entrega3.py
+++ P3_Entrega3.py
@@ -1,14 +1,14 @@
 from matplotlib.pylab import *
 
-L = 1
-n = 19
+L = 1.04
+n = 20
 dx = L / n
 
 x = linspace(0, L, n+1)
 
 
-dt = 2.
-Nt = 90000
+dt = 1.
+Nt = 5000
 u = zeros((Nt, n+1))
 
 u[:,0] = 0.		#Borde Izquierdo
@@ -24,19 +24,19 @@ alpha = K*dt/(c*rho*dx**2)
 
 eje_x=[] 
 eje_y=[]
-
 for k in range(Nt-1):
 	t = dt*k
 	print (f"k = {k} t = {t}")
-	u[k, n] = 10 * dx + u[k, n-1]
+	#u[k, n] = 10 * dx + u[k, n-1]
 	for i in range(1,n):
 		u[k+1, i] = u[k,i] + alpha*(u[k, i+1] - 2*u[k, i] + u[k, i-1])
 	print (x, u[k, :])
 
-	eje_x.append(t/(dt*3600))
+	eje_x.append(t/3600)
 	eje_y.append(u[k, 2])
 
 	if k % 500 == 0: 
+		plt.subplot(3,1,1)
 		plot(eje_x, eje_y)
 
 

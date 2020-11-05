11bca82681046d628fd6c5c8226d3d1f10e789f9
miurrutia2 <37964599+miurrutia2@users.noreply.github.com>
2020-11-05 15:27:45 -0300

    caso4

--- Entrega_5/GIFY.py
+++ Entrega_5/GIFY.py
@@ -9,8 +9,8 @@ def natural_keys(text):
     return[atoi(c) for c in re.split(r'(\d+)', text)]
 
 #Frame
-fp_in = "Caso3/frame_*.png"
-fp_out = "caso_3.gif"
+fp_in = "Caso4/frame_*.png"
+fp_out = "caso_4.gif"
 
 listaImagenes = sorted(glob.glob(fp_in))
 print("sorted(glob.glob(fp_in)): ", listaImagenes)
--- Entrega_5/caso_4.py
+++ Entrega_5/caso_4.py
@@ -138,8 +138,8 @@ for k in range(int32(Days/dt)):
     
     u_0[k] = u_k[int(Nx / 2), -1]
     u_N4[k] = u_k[int(Nx / 2), int(Ny / 4)]
-    u_2N4[k] = u_k[int(Nx / 2), int(2 * Ny / 4)]
-    u_3N4[k] = u_k[int(Nx / 2), int(3 * Ny / 4)]
+    u_2N4[k] = u_k[int(Nx / 2), int(2* Ny / 4)]
+    u_3N4[k] = u_k[int(3*Nx / 4), int(3 * Ny / 4)]
     
     #Graicando en d_next
     if t > next_t:
@@ -156,7 +156,7 @@ for k in range(int32(Days/dt)):
 figure(2)
 plot(range(int32(Days / dt)), u_0, label='Superficie')
 plot(range(int32(Days / dt)), u_N4, label='N/4')
-plot(range(int32(Days / dt)), u_2N4, label='N/2')
+plot(range(int32(Days / dt)), u_2N4, label='2N/4')
 plot(range(int32(Days / dt)), u_3N4, label='3N/4')
 title("Evolución de temperatura")
 legend()
